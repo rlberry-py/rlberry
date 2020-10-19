55feaa7d645f6a727f291972b121db9c6078cec4
yfletberliac <yannisfbe@gmail.com>
2020-10-19 11:03:40 +0200

    feat(setup): include requirements in setup and rm requirements.txt

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@ from setuptools import setup, find_packages
 
 packages = find_packages()
 
+install_requires = [
+'numpy',
+'scipy',
+'pytest',
+'numba',
+'PyOpenGL',
+'PyOpenGL_accelerate',
+'pygame'
+]
+
 setup(name='rlberry',
       version='0.0.1',
       description='Reinforcement Learning Library',
       url='https://github.com/rlberry-py',
       license='MIT',
       packages=packages,
+      install_requires=install_requires,
       zip_safe=False)
