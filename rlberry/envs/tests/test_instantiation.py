bded76fedff0c6529a77d683b13c31b924861d7c
Omar D <omar.drwch@gmail.com>
2020-10-19 15:27:46 +0200

    simple tests for environments

---
+++ rlberry/envs/tests/test_instantiation.py
@@ -0,0 +1,39 @@
+import pytest 
+
+from rlberry.envs.interface import ForwardModel, GenerativeModel
+
+from rlberry.envs.classic_control import MountainCar 
+
+from rlberry.envs.finite import GridWorld
+from rlberry.envs.finite import Chain
+
+from rlberry.envs.toy_exploration import PBall2D
+from rlberry.envs.toy_exploration import SimplePBallND
+
+
+classes = [
+            MountainCar,
+            GridWorld,
+            Chain,
+            PBall2D,
+            SimplePBallND
+          ]
+
+@pytest.mark.parametrize("ModelClass", classes)
+def test_instantiation(ModelClass):
+    env = ModelClass()
+    if isinstance(env, ForwardModel):
+        for ep in range(2):
+            state = env.reset()
+            for ii in range(50):
+                assert env.observation_space.contains(state)
+                action = env.action_space.sample()
+                next_s, reward, done, info = env.step(action)
+                state = next_s 
+
+    if isinstance(env, GenerativeModel):
+        for ii in range(100):
+            state  = env.observation_space.sample()
+            action = env.action_space.sample()
+            next_s, reward, done, info = env.sample(action, state)
+            assert env.observation_space.contains(next_s)
