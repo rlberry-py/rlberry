4d96be0cd424ac7883a953cc18d1f5ce34d9ac57
Omar D <omar.drwch@gmail.com>
2020-10-27 17:24:21 +0100

    including joblib as requirement

--- examples/demo_rs_kernel_ucbvi.py
+++ examples/demo_rs_kernel_ucbvi.py
@@ -9,7 +9,7 @@ env = Acrobot()
 env = RescaleRewardWrapper(env, (0.0, 1.0))
 
 agent = RSKernelUCBVIAgent(env, n_episodes=500, gamma=0.99, horizon=300, bonus_scale_factor=0.01,
-                           min_dist=0.25, bandwidth=0.15, beta=1.0, kernel_type="epanechnikov", verbose=4)
+                           min_dist=0.2, bandwidth=0.05, beta=1.0, kernel_type="gaussian", verbose=4)
 agent.fit()
 
 env.enable_rendering()
@@ -19,4 +19,4 @@ for tt in range(4*agent.horizon):
     next_state, reward, done, _ = env.step(action)
     state = next_state
 
-env.save_video("kernel_acrobot.mp4")
+env.save_video("kernel_acrobot_gaussian.mp4", framerate=15)
--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ install_requires = [
     'numba',
     'PyOpenGL',
     'PyOpenGL_accelerate',
-    'pygame'
+    'pygame',
+    'joblib'
 ]
 
 setup(name='rlberry',
