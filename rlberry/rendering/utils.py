fde025adb3d877b014e79084e96ce65c45fce017
Omar D <omar.drwch@gmail.com>
2020-11-25 12:23:48 +0100

    fix(rendering): video_write catches exception

--- rlberry/rendering/utils.py
+++ rlberry/rendering/utils.py
@@ -28,32 +28,37 @@ def video_write(fn, images, framerate=60, vcodec='libx264'):
     """
     global _FFMPEG_INSTALLED
 
-    if len(images) == 0:
-        logging.warning("Calling video_write() with empty images.")
-        return
+    try:
+        if len(images) == 0:
+            logging.warning("Calling video_write() with empty images.")
+            return
 
-    if not _FFMPEG_INSTALLED:
-        logging.error(
-            "video_write(): Unable to save video, ffmpeg-python \
-package required (https://github.com/kkroening/ffmpeg-python)")
-        return
+        if not _FFMPEG_INSTALLED:
+            logging.error(
+                "video_write(): Unable to save video, ffmpeg-python \
+    package required (https://github.com/kkroening/ffmpeg-python)")
+            return
 
-    if not isinstance(images, np.ndarray):
-        images = np.asarray(images)
-    n, height, width, channels = images.shape
-    process = (
-        ffmpeg
-        .input('pipe:', format='rawvideo', pix_fmt='rgb24',
-               s='{}x{}'.format(width, height), r=framerate)
-        .output(fn, pix_fmt='yuv420p', vcodec=vcodec)
-        .overwrite_output()
-        .run_async(pipe_stdin=True)
-    )
-    for frame in images:
-        process.stdin.write(
-            frame
-            .astype(np.uint8)
-            .tobytes()
+        if not isinstance(images, np.ndarray):
+            images = np.asarray(images)
+        n, height, width, channels = images.shape
+        process = (
+            ffmpeg
+            .input('pipe:', format='rawvideo', pix_fmt='rgb24',
+                s='{}x{}'.format(width, height), r=framerate)
+            .output(fn, pix_fmt='yuv420p', vcodec=vcodec)
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
         )
-    process.stdin.close()
-    process.wait()
+        for frame in images:
+            process.stdin.write(
+                frame
+                .astype(np.uint8)
+                .tobytes()
+            )
+        process.stdin.close()
+        process.wait()
+
+    except Exception as ex:
+        logging.warning("Not possible to save \
+video, due to exception: {}".format(str(ex)))
