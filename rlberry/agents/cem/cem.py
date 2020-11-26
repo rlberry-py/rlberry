e9085cc53a40596583b2ffba915e4496697fd8d4
Edouard Leurent <eleurent@gmail.com>
2020-11-26 18:03:07 +0100

    feat(DQN): Move other PG models to a common directory

--- rlberry/agents/cem/cem.py
+++ rlberry/agents/cem/cem.py
@@ -9,20 +9,9 @@ import rlberry.spaces as spaces
 from rlberry.agents import Agent
 
 # choose device
+from rlberry.agents.common.models import Net
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-class Net(nn.Module):
-    def __init__(self, obs_size, hidden_size, n_actions):
-        super(Net, self).__init__()
-        self.net = nn.Sequential(
-            nn.Linear(obs_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, n_actions)
-        )
-
-    def forward(self, x):
-        return self.net(x)
 
 
 class Memory:
--- rlberry/agents/common/models.py
+++ rlberry/agents/common/models.py
@@ -41,3 +41,59 @@ class PolicyNet(nn.Module):
     def action_scores(self, state):
         action_scores = self.actor(state)
         return action_scores
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(ActorCritic, self).__init__()
+        # actor
+        self.actor = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, action_dim),
+            nn.Softmax(dim=-1)
+        )
+        # critic
+        self.critic = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state):
+        action_probs = self.actor(state)
+        dist = Categorical(action_probs)
+        action = dist.sample()
+
+        return action, dist.log_prob(action)
+
+    def evaluate(self, state, action):
+        action_probs = self.actor(state)
+        dist = Categorical(action_probs)
+
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+
+        state_value = self.critic(state)
+
+        return action_logprobs, torch.squeeze(state_value), dist_entropy
+
+
+class Net(nn.Module):
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
