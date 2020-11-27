0655ada898efdff0449bed829cdb94aa1e064309
Edouard Leurent <eleurent@gmail.com>
2020-11-27 18:37:04 +0100

    Wrap env in DQN test
    Co-authored-by: Omar D. Domingues <omar.drwch@gmail.com>

--- rlberry/agents/tests/test_dqn.py
+++ rlberry/agents/tests/test_dqn.py
@@ -1,10 +1,10 @@
 import gym
-
+from rlberry.wrappers import Wrapper
 from rlberry.agents.dqn.pytorch import DQNAgent
 
 
 def test_dqn_agent():
-    env = gym.make("CartPole-v0")
+    env = Wrapper(gym.make("CartPole-v0"))
     agent = DQNAgent(env, {"n_episodes": 10})
     agent.fit()
     agent.policy(env.observation_space.sample())
