bac085e3af83b9a33db7e348f7dedc576d5a9392
yfletberliac <yannisfbe@gmail.com>
2020-11-30 14:30:44 +0100

    feat(partial_fit): add type float to please pycharm

--- rlberry/agents/reinforce/reinforce.py
+++ rlberry/agents/reinforce/reinforce.py
@@ -142,7 +142,7 @@ class REINFORCEAgent(IncrementalAgent):
                 "episode_rewards": self._rewards[:self.episode]}
         return info
 
-    def partial_fit(self, fraction, **kwargs):
+    def partial_fit(self, fraction: float, **kwargs):
         assert 0.0 < fraction <= 1.0
         n_episodes_to_run = int(np.ceil(fraction*self.n_episodes))
         count = 0
