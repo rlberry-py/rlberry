1188d38cd4531c335730c2ae18fb3d28be49ca7c
Omar D <omar.drwch@gmail.com>
2020-12-01 11:52:52 +0100

    feat(logging): Set default logging level to 'INFO' in rlberry/__init__.py

--- rlberry/__init__.py
+++ rlberry/__init__.py
@@ -3,3 +3,7 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 # Initialize seeding
 from rlberry.seeding import seeding
 seeding.set_global_seed()
+
+# Initialize logging level
+from rlberry.utils.logging import configure_logging
+configure_logging(level="INFO")
