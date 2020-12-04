7d7ee00fc7718c00a9b10a58210cdcfb4cf00eb2
Omar D <omar.drwch@gmail.com>
2020-12-04 14:20:15 +0100

    feat(rnd): improving example

--- examples/demo_rnd.py
+++ examples/demo_rnd.py
@@ -8,19 +8,19 @@ env = get_benchmark_env(level=1)
 rnd = RandomNetworkDistillation(
     env.observation_space,
     env.action_space,
-    learning_rate=0.01,
-    update_period=10)
+    learning_rate=0.1,
+    update_period=100)
 
 # Test
 state = env.reset()
-for ii in range(10000):
+for ii in range(20000):
     action = env.action_space.sample()
     next_s, reward, _, _ = env.step(action)
     rnd.update(state, action, next_s, reward)
     state = next_s
 
-    if ii % 100 == 0:
+    if ii % 500 == 0:
+        state = env.reset()
         bonus = rnd.measure(state, action)
         print("it = {}, bonus = {}, loss = {}"
               .format(ii, bonus, rnd.loss.item()))
-        state = env.reset()
