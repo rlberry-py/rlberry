e268970019b3710fea212f15e45b0b056ec54728
Omar D <omar.drwch@gmail.com>
2020-12-06 19:18:09 +0100

    fix(project): importing gym_make in rlberry/__init__.py to avoid circular dependencies

--- rlberry/__init__.py
+++ rlberry/__init__.py
@@ -4,6 +4,12 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from rlberry.seeding import seeding
 seeding.set_global_seed()
 
+# gym wrapper
+# Importing here avoids circular dependencies,
+# rlberry.wrappers depend on rlberry.envs,
+# and envs.gym_make depends on rlberry.wrappers
+import rlberry.envs.gym_make
+
 # Initialize logging level
 from rlberry.utils.logging import configure_logging
 configure_logging(level="INFO")
