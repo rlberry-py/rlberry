d6e3b705eb4ac22cb98e791489b0fde3899237fa
Edouard Leurent <eleurent@gmail.com>
2021-01-18 19:39:42 +0100

    feat(atari): Add atari_make function, which uses stable_baselines3 wrappers

--- rlberry/envs/gym_make.py
+++ rlberry/envs/gym_make.py
@@ -8,3 +8,11 @@ def gym_make(env_name):
     to ensure unified seeding with rlberry.
     """
     return Wrapper(gym.make(env_name))
+
+def atari_make(env_name, **kwargs):
+    from stable_baselines3.common.env_util import make_atari_env
+    from stable_baselines3.common.vec_env import VecFrameStack
+    env = make_atari_env(env_id=env_name, **kwargs)
+    env = VecFrameStack(env, n_stack=4)
+    return Wrapper(env)
+
