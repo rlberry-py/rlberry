55d28cfc35c6c0fe64a622b27d9ea6b7b8115f55
Omar D <omar.drwch@gmail.com>
2021-01-18 14:43:14 +0100

    feat(multiple_stats): Now use threads instead of processes, option to save data as soon as each thread finishes

--- rlberry/stats/multiple_stats.py
+++ rlberry/stats/multiple_stats.py
@@ -1,14 +1,17 @@
-from multiprocessing import Pool
+import concurrent.futures
 
 
-def fit_stats(stats):
+def fit_stats(stats, save):
     stats.fit()
+    if save:
+        stats.save_results()
+        stats.save()
     return stats
 
 
 class MultipleStats:
     """
-    Class to fit multiple AgentStats instances in Parallel.
+    Class to fit multiple AgentStats instances in parallel with multiple threads.
     """
     def __init__(self) -> None:
         super().__init__()
@@ -24,17 +27,35 @@ class MultipleStats:
         """
         self.instances.append(agent_stats)
 
-    def run(self, n_processes=4):
+    def run(self, n_threads=4, save=False):
         """
         Fit AgentStats instances in parallel.
 
         Parameters
         ----------
-        n_processes : int, default: 4
-            Number of parallel processes.
+        n_threads : int, default: 4
+            Number of parallel threads.
+
+        save: bool, default: False
+            If true, save AgentStats intances immediately after fitting.
+            AgentStats.save() and AgentStats.save_results() are called.
         """
-        with Pool(n_processes) as p:
-            self.instances = p.map(fit_stats, self.instances)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for inst in self.instances:
+                futures.append(
+                    executor.submit(fit_stats, inst, save=save)
+                )
+
+            fitted_instances = []
+            for future in concurrent.futures.as_completed(futures):
+                fitted_instances.append(
+                    future.result()
+                )
+
+            self.instances = fitted_instances
+        # with ThreadPool(n_processes) as p:
+        #     self.instances = p.map(fit_stats, self.instances)
 
     def save(self):
         """
