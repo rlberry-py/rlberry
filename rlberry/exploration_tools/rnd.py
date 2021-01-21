19223b58fd5dc5b649dde8987716dfde0c581045
yfletberliac <yannisfbe@gmail.com>
2021-01-21 18:08:37 +0100

    feat(rnd): add RandomNetworkDistillationWithAction taking also the action as input

--- rlberry/exploration_tools/rnd.py
+++ rlberry/exploration_tools/rnd.py
@@ -127,3 +127,93 @@ class RandomNetworkDistillation(UncertaintyEstimator):
         if not batch:
             measure = measure.item()
         return measure
+
+
+class RandomNetworkDistillationWithAction(UncertaintyEstimator):
+    """
+    References
+    ----------
+    Burda Yuri, Harrison Edwards, Amos Storkey, and Oleg Klimov. 2018.
+    "Exploration by random network distillation."
+    In International Conference on Learning Representations.
+    """
+
+    def __init__(self,
+                 observation_space,
+                 action_space,
+                 learning_rate=0.001,
+                 update_period=100,
+                 embedding_dim=10,
+                 net_fn=None,
+                 net_kwargs=None,
+                 device="cuda:best",
+                 **kwargs):
+        assert isinstance(observation_space, spaces.Box)
+        UncertaintyEstimator.__init__(self, observation_space, action_space)
+        self.learning_rate = learning_rate
+        self.loss_fn = F.mse_loss
+        self.update_period = update_period
+        self.embedding_dim = embedding_dim
+        self.net_fn = load(net_fn) if isinstance(net_fn, str) else \
+            net_fn or partial(get_network, shape=(observation_space.shape[0],
+                                                  observation_space.shape[1],
+                                                  observation_space.shape[-1]+action_space.n) , embedding_dim=embedding_dim)
+        self.net_kwargs = net_kwargs or {}
+        self.device = choose_device(device)
+        self.reset()
+
+    def reset(self, **kwargs):
+        self.random_target_network = self.net_fn(**self.net_kwargs).to(self.device)
+        self.predictor_network = self.net_fn(**self.net_kwargs).to(self.device)
+        self.rnd_optimizer = torch.optim.Adam(
+                                self.predictor_network.parameters(),
+                                lr=self.learning_rate,
+                                betas=(0.9, 0.999))
+
+        self.count = 0
+        self.loss = torch.tensor(0.0).to(self.device)
+
+    def _get_embeddings(self, state, action, batch=False):
+        state_tensor = state if isinstance(state, torch.Tensor) else torch.from_numpy(state)
+        state_tensor = state_tensor.to(self.device)
+        action_one_hot = torch.zeros((self.action_space.n))
+        action_one_hot[action] = 1.0
+        action_tensor = action_one_hot.to(self.device)
+        if not batch:
+            state_tensor = state_tensor.unsqueeze(0)
+            action_tensor = action_tensor.unsqueeze(0)
+
+        state_action_tensor = torch.cat((state_tensor, action_tensor.unsqueeze(0).unsqueeze(0).repeat(1, 84, 84, 1)), -1)
+        random_embedding = self.random_target_network(state_action_tensor)
+        predicted_embedding = self.predictor_network.forward(state_action_tensor)
+        return random_embedding, predicted_embedding
+
+    def update(self,
+               state,
+               action,
+               next_state=None,
+               reward=None,
+               **kwargs):
+        random_embedding, predicted_embedding \
+            = self._get_embeddings(state, action)
+
+        self.loss += self.loss_fn(random_embedding.detach(),
+                                  predicted_embedding)
+
+        self.count += 1
+        if self.count % self.update_period == 0:
+            self.loss /= self.update_period
+            self.rnd_optimizer.zero_grad()
+            self.loss.backward()
+            self.rnd_optimizer.step()
+            self.loss = torch.tensor(0.0).to(self.device)
+
+    def measure(self, state, action, batch=False, **kwargs):
+        random_embedding, predicted_embedding \
+            = self._get_embeddings(state, action, batch=batch)
+
+        diff = predicted_embedding.detach() - random_embedding.detach()
+        measure = torch.norm(diff, p=2, dim=1)
+        if not batch:
+            measure = measure.item()
+        return measure
