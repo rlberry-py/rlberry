bc471fba30ec435104a7cca00e43db06624b4a70
yfletberliac <yannisfbe@gmail.com>
2021-02-19 12:21:29 +0100

    feat(torch training): add some tests

--- rlberry/agents/tests/test_torch_training.py
+++ rlberry/agents/tests/test_torch_training.py
@@ -1,17 +1,18 @@
 from rlberry.agents.utils.torch_training import *
+from torch.nn import functional as F
+from rlberry.envs.benchmarks.ball_exploration.ball2d import get_benchmark_env
+from rlberry.agents.utils.torch_models import default_policy_net_fn
+
+# loss_function_factory
+assert isinstance(loss_function_factory("l2").__name__, type(F.mse_loss.__name__))
+assert isinstance(loss_function_factory("l1").__name__, type(F.l1_loss.__name__))
+assert isinstance(loss_function_factory("smooth_l1").__name__, type(F.smooth_l1_loss.__name__))
+assert isinstance(loss_function_factory("bce").__name__, type(F.binary_cross_entropy.__name__))
 
 
-# def loss_function_factory(loss_function):
-#     if loss_function == "l2":
-#         return F.mse_loss
-#     elif loss_function == "l1":
-#         return F.l1_loss
-#     elif loss_function == "smooth_l1":
-#         return F.smooth_l1_loss
-#     elif loss_function == "bce":
-#         return F.binary_cross_entropy
-#     else:
-#         raise ValueError("Unknown loss function : {}".format(loss_function))
-#
-
-print(loss_function_factory("l2"))
+# optimizer_factory
+env = get_benchmark_env(level=1)
+assert optimizer_factory(default_policy_net_fn(env).parameters(), "ADAM").defaults["lr"] == 0.001
+assert optimizer_factory(default_policy_net_fn(env).parameters(), "ADAM").defaults["betas"] == (0.9, 0.999)
+assert optimizer_factory(default_policy_net_fn(env).parameters(), "RMS_PROP").defaults["lr"] == 0.01
+assert optimizer_factory(default_policy_net_fn(env).parameters(), "RMS_PROP").defaults["alpha"] == 0.99
