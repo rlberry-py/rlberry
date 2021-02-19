c8755af38271ffa501fda811bb592d3d55c42ff7
yfletberliac <yannisfbe@gmail.com>
2021-02-19 10:46:12 +0100

    feat(trpo): add tests

--- rlberry/agents/tests/test_actor_critic_algos.py
+++ rlberry/agents/tests/test_actor_critic_algos.py
@@ -1,11 +1,9 @@
-from copy import deepcopy
 from rlberry.agents import A2CAgent
 from rlberry.agents import PPOAgent
 from rlberry.agents import AVECPPOAgent
+from rlberry.agents import TRPOAgent
 from rlberry.envs.benchmarks.ball_exploration.ball2d import get_benchmark_env
 from rlberry.exploration_tools.discrete_counter import DiscreteCounter
-from rlberry.wrappers.uncertainty_estimator_wrapper import \
-    UncertaintyEstimatorWrapper
 
 
 def test_a2c_agent():
@@ -152,6 +150,46 @@ def test_avec_ppo_agent_partial_fit():
                          k_epochs=4,
                          batch_size=1,
                          use_bonus=False)
+    agent._log_interval = 0
+
+    agent.partial_fit(0.5)
+    agent.policy(env.observation_space.sample())
+    assert agent.episode == 5
+    agent.partial_fit(0.5)
+    assert agent.episode == 10
+    agent.policy(env.observation_space.sample())
+
+
+def test_trpo_agent():
+    env = get_benchmark_env(level=1)
+    n_episodes = 5
+    horizon = 30
+
+    agent = TRPOAgent(env,
+                      n_episodes=n_episodes,
+                      horizon=horizon,
+                      gamma=0.99,
+                      learning_rate=0.001,
+                      k_epochs=4,
+                      use_bonus=False,
+                      )
+    agent._log_interval = 0
+    agent.fit()
+    agent.policy(env.observation_space.sample())
+
+
+def test_trpo_agent_partial_fit():
+    env = get_benchmark_env(level=1)
+    n_episodes = 10
+    horizon = 30
+
+    agent = TRPOAgent(env,
+                      n_episodes=n_episodes,
+                      horizon=horizon,
+                      gamma=0.99,
+                      learning_rate=0.001,
+                      k_epochs=4,
+                      use_bonus=False)
     agent._log_interval = 0
 
     agent.partial_fit(0.5)
