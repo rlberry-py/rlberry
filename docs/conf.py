f991a2210e5e0f4b1919bd225556ec0b9578d1dd
Xuedong Shang <shang.xuedong@yahoo.fr>
2021-03-15 20:39:08 +0100

    fix(docs): fix the version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, rlberry team'
 author = 'rlberry team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3.1'
+release = '0.1-beta'
 
 
 # -- General configuration ---------------------------------------------------
