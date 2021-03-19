985d3923c63ae5e6680753e586d94a696d0d21fc
Edouard Leurent <eleurent@gmail.com>
2021-03-19 19:12:28 +0100

    Add some plots to DQN

---
+++ rlberry/rendering/env_representation_render2d.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def representation_2d(state, env):
+    # If state is 2D, just use it
+    if isinstance(state, np.ndarray) and env.observation_space.shape == (2,):
+        return state
+
+    # Representation of highway-env environments
+    try:
+        from highway_env.envs.common.abstract import AbstractEnv
+        from highway_env.envs.common.observation import KinematicObservation
+        if isinstance(env.unwrapped, AbstractEnv) and \
+                isinstance(env.unwrapped.observation_type, KinematicObservation):
+            return np.array([state[0, KinematicObservation.FEATURES.index("x")],
+                            -state[0, KinematicObservation.FEATURES.index("y")]])
+    except ImportError:
+        pass
+
+    # No representation available
+    return False
