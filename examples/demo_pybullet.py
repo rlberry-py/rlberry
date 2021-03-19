1d7c4c4396d0ecbcb952a1d35679e545fba4a1c3
Edouard Leurent <eleurent@gmail.com>
2021-03-19 15:17:37 +0100

    Add some custom pybullet envs, with an example DQN + pendulum swingup

---
+++ examples/demo_pybullet.py
@@ -0,0 +1,23 @@
+from torch.utils.tensorboard import SummaryWriter
+
+import rlberry.envs.bullet3.pybullet_envs
+import space_wrappers
+import gym
+
+from rlberry.agents.torch.dqn import DQNAgent
+
+
+env = gym.make("DiscretePendulumSwingupBulletEnv-v0")
+
+env.render()
+agent = DQNAgent(env, n_episodes=100, gamma=0.95, learning_rate=0.0005, copy_env=False)
+agent.set_writer(SummaryWriter())
+agent.fit()
+
+while True:
+    state = env.reset()
+    done = False
+    while not done:
+        action = agent.policy(state)
+        next_state, reward, done, _ = env.step(action)
+        state = next_state
---
+++ rlberry/envs/bullet3/pybullet_envs/gym_pendulum_envs.py
@@ -0,0 +1,77 @@
+from gym import spaces
+from pybullet_envs.env_bases import MJCFBaseBulletEnv
+from pybullet_envs.gym_pendulum_envs import InvertedPendulumBulletEnv, InvertedPendulumSwingupBulletEnv
+from pybullet_envs.scene_abstract import SingleRobotEmptyScene
+
+from rlberry.envs.bullet3.pybullet_envs.robot_pendula import Pendulum, PendulumSwingup
+import numpy as np
+
+
+class PendulumBulletEnv(InvertedPendulumBulletEnv):
+  """Simple pendulum"""
+  def __init__(self):
+    self.robot = Pendulum()
+    MJCFBaseBulletEnv.__init__(self, self.robot)
+    self.stateId = -1
+
+  def create_single_player_scene(self, bullet_client):
+    return SingleRobotEmptyScene(bullet_client, gravity=9.81, timestep=0.02, frame_skip=1)
+
+  def step(self, a):
+    self.robot.apply_action(a)
+    self.scene.global_step()
+    state = self.robot.calc_state()  # sets self.pos_x self.pos_y
+    vel_penalty = 0
+    if self.robot.swingup:
+      reward = np.cos(self.robot.theta)
+      done = False
+    else:
+      reward = 1.0
+      done = np.abs(self.robot.theta) > .2
+    self.rewards = [float(reward)]
+    self.HUD(state, a, done)
+    return state, sum(self.rewards), done, {}
+
+
+class PendulumSwingupBulletEnv(PendulumBulletEnv):
+  def __init__(self):
+    self.robot = PendulumSwingup()
+    MJCFBaseBulletEnv.__init__(self, self.robot)
+    self.stateId = -1
+
+
+class DiscretePendulumBulletEnv(PendulumBulletEnv):
+  """pybullet's InvertedPendulum with discrete actions"""
+  def __init__(self):
+    super().__init__()
+    self.continuous_action_space = self.action_space
+    self.action_space = spaces.Discrete(3)
+
+  def step(self, a):
+    if a == 0:
+      return super().step(self.continuous_action_space.low)
+    elif a == 1:
+      return super().step(self.continuous_action_space.high)
+    elif a == 2:
+      return super().step(np.zeros(self.continuous_action_space.shape))
+    else:
+      raise IndexError
+
+
+class DiscretePendulumSwingupBulletEnv(PendulumSwingupBulletEnv):
+  """pybullet's InvertedPendulumSwingup with discrete actions"""
+  def __init__(self):
+    super().__init__()
+    self.continuous_action_space = self.action_space
+    self.action_space = spaces.Discrete(3)
+
+  def step(self, a):
+    if a == 0:
+      return super().step(self.continuous_action_space.low)
+    elif a == 1:
+      return super().step(self.continuous_action_space.high)
+    elif a == 2:
+      return super().step(np.zeros(self.continuous_action_space.shape))
+    else:
+      raise IndexError
+
