e0d368fc833f7935703b4b356189871e1424692b
Omar D <omar.drwch@gmail.com>
2021-09-07 14:55:17 +0200

    feat(gridworld): Method to return layout RGB image.

--- rlberry/envs/finite/gridworld.py
+++ rlberry/envs/finite/gridworld.py
@@ -1,5 +1,8 @@
+import matplotlib
 import numpy as np
 import logging
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 from rlberry.envs.finite import FiniteMDP
 from rlberry.rendering import Scene, GeometricPrimitive, RenderInterface2D
@@ -311,19 +314,60 @@ class GridWorld(RenderInterface2D, FiniteMDP):
         -------
         Gridworld layout array of shape (nrows, ncols).
         """
-
         layout = np.zeros((self.nrows, self.ncols))
-
         if state_data is not None:
             assert state_data.shape == (self.observation_space.n,)
             data_rows = [self.index2coord[idx][0] for idx in self.index2coord]
             data_cols = [self.index2coord[idx][1] for idx in self.index2coord]
             layout[data_rows, data_cols] = state_data
+        else:
+            state_rr, state_cc = self.index2coord[self.state]
+            layout[state_rr, state_cc] = 1.0
 
         walls_rows = [ww[0] for ww in self.walls]
         walls_cols = [ww[1] for ww in self.walls]
         layout[walls_rows, walls_cols] = fill_walls_with
         return layout
+
+    def get_layout_img(
+            self,
+            state_data=None,
+            colormap_name='cool',
+            wall_color=(0.0, 0.0, 0.0)):
+        """
+        Returns an image array representing the value of `state_data` on
+        the gridworld layout.
+
+        Parameters
+        ----------
+        state_data : np.array, default = None
+            Array of shape (self.observation_space.n,)
+        colormap_name : str, default = 'cool'
+            Colormap name.
+            See https://matplotlib.org/tutorials/colors/colormaps.html
+        wall_color : tuple
+            RGB color for walls.
+        Returns
+        -------
+        Gridworld image array of shape (nrows, ncols, 3).
+        """
+        # map data to [0.0, 1.0]
+        if state_data is not None:
+            state_data = state_data - state_data.min()
+            state_data = state_data / state_data.max()
+
+        colormap_fn = plt.get_cmap(colormap_name)
+        layout = self.get_layout_array(state_data, fill_walls_with=np.nan)
+        norm = matplotlib.colors.Normalize(vmin=0.0, vmax=1.0)
+        scalar_map = cm.ScalarMappable(norm=norm, cmap=colormap_fn)
+        img = np.zeros(layout.shape + (3,))
+        for rr in range(layout.shape[0]):
+            for cc in range(layout.shape[1]):
+                if np.isnan(layout[rr, cc]):
+                    img[rr, cc, :] = wall_color
+                else:
+                    img[rr, cc, :3] = scalar_map.to_rgba(layout[rr, cc])[:3]        
+        return img
 
     def get_background(self):
         """
