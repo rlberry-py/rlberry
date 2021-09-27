6a330d0c1501716fbe437b8e59265175bed3c3d1
Omar D <omar.drwch@gmail.com>
2021-09-27 14:26:43 +0200

    update example

--- examples/demo_network/run_remote_manager.py
+++ examples/demo_network/run_remote_manager.py
@@ -1,10 +1,25 @@
 from rlberry.network.client import BerryClient
 from rlberry.network.interface import ResourceRequest
 
+from rlberry.agents import ValueIterationAgent as LocalValueIterationAgent
+from rlberry.envs.finite import GridWorld as LocalGridWorld
+
+from rlberry.manager.agent_manager import AgentManager
+from rlberry.manager.multiple_managers import MultipleManagers
 from rlberry.manager.remote_agent_manager import RemoteAgentManager
 
-
 client = BerryClient()
+
+
+local_stats = AgentManager(
+    agent_class=LocalValueIterationAgent,
+    train_env=(LocalGridWorld, dict(nrows=35)),
+    fit_budget=100,
+    init_kwargs=dict(gamma=0.99),
+    eval_kwargs=dict(eval_horizon=100, n_simulations=20),
+    n_fit=2,
+    seed=10
+)
 
 
 remote_stats = RemoteAgentManager(
@@ -18,7 +33,10 @@ remote_stats = RemoteAgentManager(
     seed=10
 )
 
+mmanagers = MultipleManagers()
+mmanagers.append(local_stats)
+mmanagers.append(remote_stats)
 
-remote_stats.fit()
+mmanagers.run()
 
-print(remote_stats.eval())
+print([stats.eval() for stats in mmanagers.managers])
