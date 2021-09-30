ab0248e53864859b410b6df5c0fa6b26d068b158
Omar D <omar.drwch@gmail.com>
2021-09-30 21:51:48 +0200

    fix(example)

--- examples/demo_rs_kernel_ucbvi.py
+++ examples/demo_rs_kernel_ucbvi.py
@@ -6,11 +6,11 @@ env = Acrobot()
 # rescake rewards to [0, 1]
 env = RescaleRewardWrapper(env, (0.0, 1.0))
 
-agent = RSKernelUCBVIAgent(env, n_episodes=500, gamma=0.99, horizon=300,
+agent = RSKernelUCBVIAgent(env, gamma=0.99, horizon=300,
                            bonus_scale_factor=0.01,
                            min_dist=0.2, bandwidth=0.05, beta=1.0,
                            kernel_type="gaussian")
-agent.fit()
+agent.fit(budget=500)
 
 env.enable_rendering()
 state = env.reset()
