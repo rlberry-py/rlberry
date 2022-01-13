ee489ace48d56f8a698c6ab6064ec625deb6ac16
menardprr <menardprr@gmail.com>
2022-01-13 13:47:20 +0000

    add PSRL agent for tabular MDP

---
+++ rlberry/agents/tests/test_psrl.py
@@ -0,0 +1,20 @@
+import pytest
+from rlberry.agents.psrl import PSRLAgent
+from rlberry.envs.finite import GridWorld
+
+
+@pytest.mark.parametrize("gamma, stage_dependent",
+                         [
+                             (1.0, True),
+                             (1.0, False),
+                             (0.9, True),
+                             (0.9, False),
+                         ])
+def test_ucbvi(gamma, stage_dependent):
+    env = GridWorld(walls=(), nrows=5, ncols=5)
+    agent = PSRLAgent(env,
+                       horizon=11,
+                       stage_dependent=stage_dependent,
+                       gamma=gamma)
+    agent.fit(budget=50)
+    agent.policy(env.observation_space.sample())
