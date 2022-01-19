e903d8c7bf6828d365d41193d16c94db2ec62a09
Pierre Ménard <menardprr@gmail.com>
2022-01-19 18:51:53 +0100

    feat(agent): add RLSVI algorithm plus option for PSLR (#101)
    * add RLSVI algorithm plus option for PSLR
    
    * change the std of the noise to simplified bernstein shape
    
    * fix(style): use black
    
    * fix(style)
    
    * fix(style)
    
    Co-authored-by: Omar D <omar.drwch@gmail.com>

--- rlberry/agents/tests/test_psrl.py
+++ rlberry/agents/tests/test_psrl.py
@@ -4,16 +4,26 @@ from rlberry.envs.finite import GridWorld
 
 
 @pytest.mark.parametrize(
-    "gamma, stage_dependent",
+    "gamma, stage_dependent, bernoullized_reward",
     [
-        (1.0, True),
-        (1.0, False),
-        (0.9, True),
-        (0.9, False),
+        (1.0, True, True),
+        (1.0, True, False),
+        (1.0, False, True),
+        (1.0, False, False),
+        (0.9, True, True),
+        (0.9, True, False),
+        (0.9, False, True),
+        (0.9, False, False),
     ],
 )
-def test_ucbvi(gamma, stage_dependent):
+def test_ucbvi(gamma, stage_dependent, bernoullized_reward):
     env = GridWorld(walls=(), nrows=5, ncols=5)
-    agent = PSRLAgent(env, horizon=11, stage_dependent=stage_dependent, gamma=gamma)
+    agent = PSRLAgent(
+        env,
+        horizon=11,
+        bernoullized_reward=bernoullized_reward,
+        stage_dependent=stage_dependent,
+        gamma=gamma,
+    )
     agent.fit(budget=50)
     agent.policy(env.observation_space.sample())
