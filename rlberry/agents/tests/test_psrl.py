5cbeb9086c982251b8d089e109d0585ccbd4d081
Omar D <omar.drwch@gmail.com>
2022-01-19 17:02:23 +0100

    fix(style)

--- rlberry/agents/tests/test_psrl.py
+++ rlberry/agents/tests/test_psrl.py
@@ -3,17 +3,19 @@ from rlberry.agents.psrl import PSRLAgent
 from rlberry.envs.finite import GridWorld
 
 
-@pytest.mark.parametrize("gamma, stage_dependent, bernoullized_reward",
-                         [
-                             (1.0, True, True),
-                             (1.0, True, False),
-                             (1.0, False, True),
-                             (1.0, False, False),
-                             (0.9, True, True),
-                             (0.9, True, False),
-                             (0.9, False, True),
-                             (0.9, False, False),
-                         ])
+@pytest.mark.parametrize(
+    "gamma, stage_dependent, bernoullized_reward",
+    [
+        (1.0, True, True),
+        (1.0, True, False),
+        (1.0, False, True),
+        (1.0, False, False),
+        (0.9, True, True),
+        (0.9, True, False),
+        (0.9, False, True),
+        (0.9, False, False),
+    ],
+)
 def test_ucbvi(gamma, stage_dependent, bernoullized_reward):
     env = GridWorld(walls=(), nrows=5, ncols=5)
     agent = PSRLAgent(
@@ -21,6 +23,7 @@ def test_ucbvi(gamma, stage_dependent, bernoullized_reward):
         horizon=11,
         bernoullized_reward=bernoullized_reward,
         stage_dependent=stage_dependent,
-        gamma=gamma)
+        gamma=gamma,
+    )
     agent.fit(budget=50)
     agent.policy(env.observation_space.sample())
