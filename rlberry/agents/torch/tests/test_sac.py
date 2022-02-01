fac9e8c0aa16d66c45e32441e1e564aea3216a8a
riccardo <riccardo.dellavecchia91@gmail.com>
2022-02-01 17:17:42 +0100

    added the horizon as terminal state

--- rlberry/agents/torch/tests/test_sac.py
+++ rlberry/agents/torch/tests/test_sac.py
@@ -50,3 +50,6 @@ def test_sac_agent_partial_fit():
     agent.fit(budget=n_episodes // 2)
     assert agent.episode == 10
     agent.policy(env.observation_space.sample())
+
+
+test_sac_agent_partial_fit()
