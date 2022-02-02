da04f3b9aea215dd3b062d064d2a713b9aaa8324
TimotheeMathieu <timothee.mathieu@u-psud.fr>
2022-02-02 11:44:40 +0100

    add lsviucb

--- rlberry/tests/test_agent.py
+++ rlberry/tests/test_agent.py
@@ -5,13 +5,34 @@ from rlberry.utils.check_discrete_action_agent import (
     check_finiteMDP_agent,
     check_continuous_state_agent,
 )
+from rlberry.agents.features import FeatureMap
+import numpy as np
+
+
+class OneHotFeatureMap(FeatureMap):
+    def __init__(self, S, A):
+        self.S = S
+        self.A = A
+        self.shape = (S * A,)
+
+    def map(self, observation, action):
+        feat = np.zeros((self.S, self.A))
+        feat[observation, action] = 1.0
+        return feat.flatten()
+
+class OneHotLSVI(LSVIUCBAgent):
+    def __init__(self, env, **kwargs):
+        def feature_map_fn(_env):
+            return OneHotFeatureMap(5,2) # values for Chain
+        LSVIUCBAgent.__init__(self, env, feature_map_fn=feature_map_fn, horizon=10, **kwargs)
+
 
 FINITE_MDP_AGENTS = [
     ValueIterationAgent,
     MBQVIAgent,
     UCBVIAgent,
     OptQLAgent,
-    # LSVIUCBAgent, # for now excluded because feature map not specified
+    OneHotLSVI
 ]
 
 
