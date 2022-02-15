e4e1e428eddb32e30a628d8c5769276fa1a1d7d1
TimotheeMathieu <timothee.mathieu@u-psud.fr>
2022-02-15 18:19:20 +0100

    improve checks

--- rlberry/utils/check_agent.py
+++ rlberry/utils/check_agent.py
@@ -3,11 +3,24 @@ from rlberry.envs.benchmarks.ball_exploration import PBall2D
 from rlberry.manager import AgentManager
 import numpy as np
 from rlberry.seeding import set_external_seed
+import tempfile
+import os
 
 SEED = 42
 
 
-def _run_agent_manager(Agent, continuous_state=False):
+def check_agent_manager(Agent, continuous_state=False):
+    """
+    Check that the agent is compatible with :class:`~rlberry.manager.AgentManager`.
+    Parameters
+    ----------
+
+    Agent: rlberry agent module
+        Agent class that we want to test.
+
+    continuous_state: bool, default=False
+        whether we test on a discrete state environment or a continuous one.
+    """
     if continuous_state:
         env_ctor = PBall2D
     else:
@@ -25,6 +38,23 @@ def _run_agent_manager(Agent, continuous_state=False):
 
 
 def check_agent_almost_equal(state, agent1, agent2, n_checks=5):
+    """
+    Check that two agents are almost equal in the sense that they give the same
+    actions n_checks times on a given state.
+
+
+    Parameters
+    ----------
+
+    state: state
+        environment state
+    agent1: rlberry agent
+        first agent to be compared
+    agent2: rlberry agent
+        second agent to be compared
+    n_checks : int, default=5
+        number of checks to do.
+    """
     result = True
     set_external_seed(SEED)
     actions1 = []
@@ -41,6 +71,19 @@ def check_agent_almost_equal(state, agent1, agent2, n_checks=5):
 
 
 def check_fit_additive(Agent, continuous_state=False):
+    """
+    check that fitting two times with 10 fit budget is the same as fitting
+    one time with 20 fit budget.
+
+    Parameters
+    ----------
+
+    Agent: rlberry agent module
+        Agent class that we want to test.
+
+    continuous_state: bool, default=False
+        whether we test on a discrete state environment or a continuous one.
+    """
     if continuous_state:
         env_ctor = PBall2D
     else:
@@ -66,10 +109,60 @@ def check_fit_additive(Agent, continuous_state=False):
     ), "Error: fitting the agent two times for 10 steps is not equivalent to fitting it one time for 20 steps."
 
 
-def check_seeding_agent(Agent, continuous_state=False):
+def check_save(Agent, continuous_state=False):
+    """
+    Check that the agent save a non-empty file.
 
-    agent1 = _run_agent_manager(Agent, continuous_state)
-    agent2 = _run_agent_manager(Agent, continuous_state)
+
+    Parameters
+    ----------
+
+    Agent: rlberry agent module
+        Agent class that we want to test.
+
+    continuous_state: bool, default=False
+        whether we test on a discrete state environment or a continuous one.
+    """
+    if continuous_state:
+        env_ctor = PBall2D
+    else:
+        env_ctor = Chain
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        if continuous_state:
+            env_ctor = PBall2D
+        else:
+            env_ctor = Chain
+        env_kwargs = {}
+        agent1 = AgentManager(
+            Agent,
+            (env_ctor, env_kwargs),
+            fit_budget=5,
+            n_fit=1,
+            seed=SEED,
+            output_dir=tmpdirname,
+        )
+        agent1.fit(10)
+        assert (
+            os.path.getsize(str(agent1.output_dir_) + "/agent_handlers/idx_0.pickle")
+            > 1
+        ), "the file saved appear to be empty"
+
+
+def check_seeding_agent(Agent, continuous_state=False):
+    """
+    Check that the agent is reproducible.
+
+    Parameters
+    ----------
+
+    Agent: rlberry agent module
+        Agent class that we want to test.
+
+    continuous_state: bool, default=False
+        whether we test on a discrete state environment or a continuous one.
+    """
+    agent1 = check_agent_manager(Agent, continuous_state)
+    agent2 = check_agent_manager(Agent, continuous_state)
 
     if continuous_state:
         env_ctor = PBall2D
@@ -97,6 +190,7 @@ def check_rl_agent(Agent, continuous_state=False):
         whether we test on a discrete state environment or a continuous one.
 
     """
-    _run_agent_manager(Agent, continuous_state)  # check manager compatible.
+    check_agent_manager(Agent, continuous_state)  # check manager compatible.
     check_seeding_agent(Agent, continuous_state)  # check reproducibility
     check_fit_additive(Agent, continuous_state)
+    check_save(Agent, continuous_state)
