4519c9edb0425dc2d206baa5e09eb219728e0d13
TimotheeMathieu <timothee.mathieu@u-psud.fr>
2022-02-17 20:05:45 +0100

    correct gym test

--- rlberry/utils/tests/test_check.py
+++ rlberry/utils/tests/test_check.py
@@ -1,21 +1,32 @@
 import numpy as np
 import pytest
 from rlberry.envs import GridWorld, Chain
-from gym.utils.env_checker import check_env
+from rlberry.utils.check_env import check_env
 from rlberry.utils.check_agent import (
     check_rl_agent,
     _fit_agent_manager,
     check_agents_almost_equal,
 )
+from gym.spaces import Box, Dict, Discrete
+import gym
 from rlberry.agents import ValueIterationAgent, UCBVIAgent
 
 
-class TestEnv(Chain):
+class ActionDictTestEnv(gym.Env):
+    action_space = Dict({"position": Discrete(1), "velocity": Discrete(1)})
+    observation_space = Box(low=-1.0, high=2.0, shape=(3,), dtype=np.float32)
+
     def step(self, action):
         observation = np.array([1.0, 1.5, 0.5])
         reward = 1
         done = True
         return observation, reward, done
+
+    def reset(self):
+        return np.array([1.0, 1.5, 0.5])
+
+    def render(self, mode="human"):
+        pass
 
 
 class TestAgent(ValueIterationAgent):
@@ -43,10 +54,10 @@ class TestAgent(ValueIterationAgent):
 
 def test_check_env_dict_action():
     # Environment.step() only returns 3 values: obs, reward, done. Not info!
-    test_env = TestEnv()
+    test_env = ActionDictTestEnv()
 
     with pytest.raises(AssertionError) as errorinfo:
-        check_env(env=test_env, warn=True)
+        check_env(env=test_env)
         assert (
             str(errorinfo.value)
             == "The `step()` method must return four values: obs, reward, done, info"
