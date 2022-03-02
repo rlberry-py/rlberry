633c80762fccf5d16bc62d495474021c909fe510
TimotheeMathieu <timothee.mathieu@inria.fr>
2022-03-02 10:59:06 +0100

    initial commit

--- rlberry/agents/bandits/__init__.py
+++ rlberry/agents/bandits/__init__.py
@@ -2,3 +2,4 @@ from .bandit_base import BanditWithSimplePolicy
 from .index_agents import IndexAgent, RecursiveIndexAgent
 from .randomized_agents import RandomizedAgent
 from .thompson_sampling import TSAgent
+from .track_and_stop_agents import TrackAndStopAgent
