c9ced14b0a6574d5ccb02c04ae0b3cd1c669d4df
Riccardo Della Vecchia <riccardo.dellavecchia91@gmail.com>
2022-03-02 10:33:04 +0100

    [WIP] sac with replay buffer (#138)
    * implemented replay buffer
    
    * some important fixes
    
    * fix test_sac.py

--- examples/demo_agents/video_plot_SAC.py
+++ examples/demo_agents/video_plot_SAC.py
@@ -8,48 +8,57 @@ during the fit of the agent.
 """
 
 
-import numpy as np
+# import numpy as np
+# from rlberry.wrappers import WriterWrapper
+from rlberry.envs.basewrapper import Wrapper
 
-from rlberry.wrappers import WriterWrapper
-
-# from rlberry.envs import GridWorld
+# from rlberry.envs import gym_make
 from rlberry.manager import plot_writer_data, AgentManager
-
-
 from rlberry.envs.benchmarks.ball_exploration import PBall2D
-
-# from rlberry.agents.torch.a2c.a2c import A2CAgent
 from rlberry.agents.torch import SACAgent
-
-# We wrape the default writer of the agent in a WriterWrapper
-# to record rewards.
+import gym
 
 
-class SSACAgent(SACAgent):
-    name = "SACAgent"
-
-    def __init__(self, env, **kwargs):
-        SACAgent.__init__(self, env, horizon=100, batch_size=5, **kwargs)
-        self.env = WriterWrapper(self.env, self.writer, write_scalar="reward")
+# we dont need wrapper actually just 'return env' works
+def env_ctor(env, wrap_spaces=True):
+    return Wrapper(env, wrap_spaces)
 
 
-env_ctor = PBall2D
-env_kwargs = dict()
-env = env_ctor(**env_kwargs)
+env = PBall2D()
+env = gym.wrappers.TimeLimit(env, max_episode_steps=100)
+env_kwargs = dict(env=env)
+agent = AgentManager(
+    SACAgent,
+    (env_ctor, env_kwargs),
+    fit_budget=500,
+    n_fit=1,
+    enable_tensorboard=True,
+)
 
-agent = AgentManager(SSACAgent, (env_ctor, env_kwargs), fit_budget=100, n_fit=3)
+# basic version
+# env_kwargs = dict(id = "CartPole-v0")
+# agent = AgentManager(SACAgent, (gym_make, env_kwargs), fit_budget=200, n_fit=1)
+
+# # timothe's
+# env = gym_make("CartPole-v0")
+# agent = AgentManager(
+#     SACAgent, (env.__class__, dict()), fit_budget=200, n_fit=1, enable_tensorboard=True,
+# )
+
+# Omar's
+# env = gym_make("CartPole-v0")
+# from copy import deepcopy
+# def env_constructor():
+#     return deepcopy(env)
+# agent = AgentManager(
+#     SACAgent, (env_constructor, dict()), fit_budget=200, n_fit=1, enable_tensorboard=True,
+# )
+
+
 agent.fit()
-
-# We use the following preprocessing function to plot the cumulative reward.
-def compute_reward(rewards):
-    return np.cumsum(rewards)
-
 
 # Plot of the cumulative reward.
 output = plot_writer_data(agent, tag="loss_q1", title="Loss q1")
-
 output = plot_writer_data(agent, tag="loss_q2", title="Loss q2")
-
 output = plot_writer_data(agent, tag="loss_v", title="Loss critic")
-
 output = plot_writer_data(agent, tag="loss_act", title="Loss actor")
