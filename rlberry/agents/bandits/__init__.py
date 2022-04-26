9c9512f1cbe89f0024c5114365cb9238cae1f71d
TMat <timothee.mathieu@inria.fr>
2022-04-26 16:00:59 +0200

    put everything in examples

--- rlberry/agents/bandits/__init__.py
+++ rlberry/agents/bandits/__init__.py
@@ -2,4 +2,3 @@ from .bandit_base import BanditWithSimplePolicy
 from .index_agents import IndexAgent, RecursiveIndexAgent
 from .randomized_agents import RandomizedAgent
 from .thompson_sampling import TSAgent
-from .bai_agents import SeqHalvAgent
--- rlberry/envs/bandits/__init__.py
+++ rlberry/envs/bandits/__init__.py
@@ -1,4 +1,3 @@
 from .bandit_base import AdversarialBandit, Bandit
 from .corrupted_bandits import CorruptedLaws, CorruptedNormalBandit
 from .stochastic_bandits import BernoulliBandit, NormalBandit
-from .mirror_env import MirrorBandit
