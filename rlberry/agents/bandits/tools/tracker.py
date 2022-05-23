903d0a3cb5dab10d605d34671b8bd845ec71aff4
TimotheeMathieu <timothee.mathieu@inria.fr>
2022-05-23 10:10:22 +0200

    add print_log argument to writer

--- rlberry/agents/bandits/tools/tracker.py
+++ rlberry/agents/bandits/tools/tracker.py
@@ -45,7 +45,7 @@ class BanditTracker(DefaultWriter):
             maxlen=maxlen,
             maxlen_by_tag=maxlen_by_tag,
         )
-        DefaultWriter.__init__(self, **_tracker_kwargs)
+        DefaultWriter.__init__(self, print_log=False, **_tracker_kwargs)
 
         self.reset_tracker()
 
--- rlberry/manager/agent_manager.py
+++ rlberry/manager/agent_manager.py
@@ -474,6 +474,7 @@ class AgentManager:
         """
         return self.default_writer_data
 
+
     def get_agent_instances(self):
         """Returns a list containing ``n_fit`` agent instances.
 
