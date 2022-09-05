fab3a55d44ca0419e967cb01283e803615efca96
AleShi94 <alena.shilova@skolkovotech.ru>
2022-09-05 11:07:40 +0200

    Double Cart Pole with a Spring env (#235)
    * Working spring environment
    
    * Corrected obs_trans calculations, added documentation for SpringCartPole and corresponding test
    
    * blacked
    
    * corrected dimensions
    
    * less heavy test
    
    * deleted useless variables
    
    * blacked
    
    * better coverage
    
    * add video example and add to api
    
    * Changed the default values to a more simple case, better demo video
    
    * Timothee's suggestions
    
    * dummy commiit
    
    Co-authored-by: SHILOVA Alena <alena.shilova@inria.fr>
    Co-authored-by: riccardo <riccardo.dellavecchia91@gmail.com>
    Co-authored-by: Alena Shilova <ashilova@smithers.lille.inria.fr>
    Co-authored-by: TimotheeMathieu <timothee.mathieu@inria.fr>

---
+++ examples/demo_env/video_plot_springcartpole.py
@@ -0,0 +1,47 @@
+"""
+===============================================
+A demo of SpringCartPole environment with DQNAgent
+===============================================
+Illustration of the training and video rendering of DQN Agent in
+SpringCartPole environment.
+
+Agent is slightly tuned, but not optimal. This is just for illustration purpose.
+
+.. video:: ../../video_plot_springcartpole.mp4
+   :width: 600
+
+"""
+# sphinx_gallery_thumbnail_path = 'thumbnails/video_plot_springcartpole.jpg'
+
+from rlberry.envs.classic_control import SpringCartPole
+from rlberry.agents.torch import DQNAgent
+from gym.wrappers.time_limit import TimeLimit
+
+model_configs = {
+    "type": "MultiLayerPerceptron",
+    "layer_sizes": (256, 256),
+    "reshape": False,
+}
+
+init_kwargs = dict(
+    q_net_constructor="rlberry.agents.torch.utils.training.model_factory_from_env",
+    q_net_kwargs=model_configs,
+)
+
+env = SpringCartPole(obs_trans=False, swing_up=True)
+env = TimeLimit(env, max_episode_steps=500)
+agent = DQNAgent(env, **init_kwargs)
+agent.fit(budget=1e5)
+
+env.enable_rendering()
+state = env.reset()
+
+for tt in range(1000):
+    action = agent.policy(state)
+    next_state, reward, done, _ = env.step(action)
+    if done:
+        next_state = env.reset()
+    state = next_state
+
+# Save video
+video = env.save_video("_video/video_plot_springcartpole.mp4")
---
+++ rlberry/envs/tests/test_spring_env.py
@@ -0,0 +1,83 @@
+import numpy as np
+from rlberry.envs import SpringCartPole
+
+
+# # actions
+# LL = 0
+# RR = 1
+# LR = 2
+# RL = 3
+
+# action_dict = {0: "LL", 1: "RR", 2: "LR", 3: "RL"}
+
+
+HORIZON = 20
+
+
+def test_spring_cartpole():
+
+    # test 1 - default
+    env = SpringCartPole()
+
+    _ = env.reset()
+    for _ in range(2):
+        action = np.random.randint(0, env.action_space.n)
+        next_state, _, done, _ = env.step(action)
+        # if done:
+        #     next_state = env.reset()
+        # state = next_state
+
+    # test 2 - obs_trans = True and random_init = False
+    env = SpringCartPole(obs_trans=True, random_init=False)
+
+    _ = env.reset()
+    for _ in range(2):
+        action = np.random.randint(0, env.action_space.n)
+        next_state, _, done, _ = env.step(action)
+        # if done:
+        #     next_state = env.reset()
+        # state = next_state
+
+    # # test 3 - swingup = False and random_init = False
+    # env = SpringCartPole(dt=0.01, swing_up=False, random_init=False)
+    # # env.enable_rendering()
+
+    # state = env.reset()
+    # for tt in range(5):
+    #     if state[2] > 0:
+    #         if state[6] > 0:
+    #             action = LL
+    #         else:
+    #             action = LR
+    #     else:
+    #         if state[6] > 0:
+    #             action = RL
+    #         else:
+    #             action = RR
+    #     # print("Time: ", tt, "Action: ", action_dict[action], "Angle1: ", state[2], "Angle2: ", state[6])
+    #     next_state, reward, done, _ = env.step(action)
+    #     if done:
+    #         next_state = env.reset()
+    #     state = next_state
+
+    # test 4 - swingup = False and rendering = True
+
+    env = SpringCartPole(dt=0.02, swing_up=False, obs_trans=True)
+    env.enable_rendering()
+
+    _ = env.reset()
+    action = 0
+    for _ in range(2 * HORIZON):
+        next_state, _, done, _ = env.step(action)
+        if done:
+            action += 1
+            if action >= 4:
+                action = 0
+            next_state = env.reset()
+        _ = next_state
+
+    _ = env.get_video()
+
+
+if __name__ == "__main__":
+    test_spring_cartpole()
