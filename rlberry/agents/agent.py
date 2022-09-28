9e53c721fa352cf0bfd685321aab98de86c508ae
TimotheeMathieu <timothee.mathieu@inria.fr>
2022-09-28 20:06:01 +0200

    bzip in agent

--- rlberry/agents/agent.py
+++ rlberry/agents/agent.py
@@ -340,7 +340,6 @@ class Agent(ABC):
         filename = Path(filename).with_suffix(".pickle")
         obj = cls(**kwargs)
 
-        obj = cls(**kwargs)
         try:
             if not obj.compress_pickle:
                 with filename.open("rb") as ff:
