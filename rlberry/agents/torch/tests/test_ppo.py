7e280101e7fe62238102ff5cf51b15e0f4d0b1c3
JulienT01 <julien.tj@gmail.com>
2023-03-31 15:43:53 +0200

    allow to continue the buffer after a first 'fit()'

--- rlberry/agents/torch/tests/test_ppo.py
+++ rlberry/agents/torch/tests/test_ppo.py
@@ -7,12 +7,15 @@
 # ppo = PPOAgent(env)
 # ppo.fit(4096)
 
-from rlberry.envs import Wrapper
+from rlberry.envs import Wrapper, gym_make
 from rlberry.agents.torch import PPOAgent
 from rlberry.manager import AgentManager, evaluate_agents
 from rlberry.envs.benchmarks.ball_exploration import PBall2D
 from gym import make
 from rlberry.agents.torch.utils.training import model_factory_from_env
+import os
+import pathlib
+import shutil
 
 
 def test_ppo():
@@ -24,7 +27,7 @@ def test_ppo():
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(batch_size=24, n_steps=96),
         n_fit=1,
@@ -44,7 +47,7 @@ def test_ppo():
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(batch_size=24, n_steps=96),
         n_fit=1,
@@ -64,7 +67,7 @@ def test_ppo():
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(batch_size=24, n_steps=96),
         n_fit=1,
@@ -78,11 +81,10 @@ def test_ppo():
 
     env_ctor = PBall2D
     env_kwargs = dict()
-
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(batch_size=24, n_steps=96),
         n_fit=1,
@@ -103,7 +105,7 @@ def test_ppo():
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(
             batch_size=24,
@@ -133,7 +135,7 @@ def test_ppo():
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(
             batch_size=24,
@@ -165,11 +167,10 @@ def test_ppo():
 
     env_ctor = PBall2D
     env_kwargs = dict()
-
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(batch_size=24, n_steps=96),
         n_fit=1,
@@ -184,7 +185,7 @@ def test_ppo():
     pporlberry_stats = AgentManager(
         PPOAgent,
         (env_ctor, env_kwargs),
-        fit_budget=int(100),
+        fit_budget=int(1000),
         eval_kwargs=dict(eval_horizon=2),
         init_kwargs=dict(batch_size=24, n_steps=96, normalize_advantages=True),
         n_fit=1,
@@ -197,5 +198,380 @@ def test_ppo():
     pporlberry_stats.clear_output_dir()
 
 
-if __name__ == "__main__":
-    test_ppo()
+def test_ppo_single_env():
+    env = gym_make("CartPole-v0")
+    agent = PPOAgent(
+        env,
+        learning_rate=1e-4,
+        optimizer_type="ADAM",
+    )
+    agent.fit(budget=1000)
+
+    saving_path = "rlberry/agents/torch/tests/agent_test_ppo_classic_env.pickle"
+
+    # VRemove previous save
+    if os.path.exists(saving_path):
+        os.remove(saving_path)
+    assert not os.path.exists(saving_path)
+
+    # test the save function
+    agent.save(saving_path)
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    loaded_agent = PPOAgent.load(saving_path, **dict(env=test_load_env))
+    assert loaded_agent
+
+    # test the agent
+    observation = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent.policy(observation)
+        next_observation, reward, done, info = test_load_env.step(
+            action
+        )
+        if done:
+            next_observation = test_load_env.reset()
+        observation = next_observation
+
+    os.remove(saving_path)
+
+
+
+def test_ppo_multi_env():
+    env = gym_make("CartPole-v0")
+    agent = PPOAgent(
+        env,
+        learning_rate=1e-4,
+        optimizer_type="ADAM",
+        n_envs=3
+    )
+    agent.fit(budget=1000)
+
+    saving_path = "rlberry/agents/torch/tests/agent_test_ppo_classic_env.pickle"
+
+    # VRemove previous save
+    if os.path.exists(saving_path):
+        os.remove(saving_path)
+    assert not os.path.exists(saving_path)
+
+    # test the save function
+    agent.save(saving_path)
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    loaded_agent = PPOAgent.load(saving_path, **dict(env=test_load_env))
+    assert loaded_agent
+
+    # test the agent
+    observation = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent.policy(observation)
+        next_observation, reward, done, info = test_load_env.step(
+            action
+        )
+        if done:
+            next_observation = test_load_env.reset()
+        observation = next_observation
+
+    os.remove(saving_path)
+
+
+def test_ppo_multi_fit():
+    env = gym_make("CartPole-v0")
+    agent = PPOAgent(
+        env,
+        learning_rate=1e-4,
+        optimizer_type="ADAM",
+    )
+    agent.fit(budget=1000)
+    agent.fit(budget=1000)
+
+    saving_path = "rlberry/agents/torch/tests/agent_test_ppo_classic_env.pickle"
+
+    # VRemove previous save
+    if os.path.exists(saving_path):
+        os.remove(saving_path)
+    assert not os.path.exists(saving_path)
+
+    # test the save function
+    agent.save(saving_path)
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    loaded_agent = PPOAgent.load(saving_path, **dict(env=test_load_env))
+    assert loaded_agent
+
+    # test the agent
+    observation = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent.policy(observation)
+        next_observation, reward, done, info = test_load_env.step(
+            action
+        )
+        if done:
+            next_observation = test_load_env.reset()
+        observation = next_observation
+
+    os.remove(saving_path)
+
+
+def test_ppo_multi_env_multi_fit():
+    env = gym_make("CartPole-v0")
+    agent = PPOAgent(
+        env,
+        learning_rate=1e-4,
+        optimizer_type="ADAM",
+        n_envs=3
+    )
+    agent.fit(budget=1000)
+    agent.fit(budget=1000)
+
+    saving_path = "rlberry/agents/torch/tests/agent_test_ppo_classic_env.pickle"
+
+    # VRemove previous save
+    if os.path.exists(saving_path):
+        os.remove(saving_path)
+    assert not os.path.exists(saving_path)
+
+    # test the save function
+    agent.save(saving_path)
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    loaded_agent = PPOAgent.load(saving_path, **dict(env=test_load_env))
+    assert loaded_agent
+
+    # test the agent
+    observation = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent.policy(observation)
+        next_observation, reward, done, info = test_load_env.step(
+            action
+        )
+        if done:
+            next_observation = test_load_env.reset()
+        observation = next_observation
+
+    os.remove(saving_path)
+
+def test_ppo_agent_manager_single_env():
+    saving_path = "rlberry/agents/torch/tests/agentmanager_test_ppo_classic_env"
+
+    # Remove previous save
+    if os.path.exists(saving_path):
+        shutil.rmtree(saving_path)
+    assert not os.path.exists(saving_path)
+
+    test_agent_manager = AgentManager(
+        PPOAgent,  # The Agent class.
+        (
+            gym_make,
+            dict(
+                id="CartPole-v0",
+            ),
+        ),  # The Environment to solve.
+        init_kwargs=dict(  # Where to put the agent's hyperparameters
+            learning_rate=1e-4,
+            optimizer_type="ADAM",
+        ),
+        fit_budget=1000,  # The number of interactions between the agent and the environment during training.
+        eval_kwargs=dict(
+            eval_horizon=50
+        ),  # The number of interactions between the agent and the environment during evaluations.
+        n_fit=1,  # The number of agents to train. Usually, it is good to do more than 1 because the training is stochastic.
+        agent_name="test_ppo_classic_env",  # The agent's name.
+        output_dir=saving_path,
+    )
+
+    test_agent_manager.fit(budget=1000)
+
+    # test the save function
+    test_agent_manager.save()
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    path_to_load = next(pathlib.Path(saving_path).glob("**/*.pickle"))
+    loaded_agent_manager = AgentManager.load(path_to_load)
+    assert loaded_agent_manager
+
+    # test the agent
+    state = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent_manager.get_agent_instances()[0].policy(state)
+        next_s, _, done, test = test_load_env.step(action)
+        if done:
+            break
+        state = next_s
+
+    shutil.rmtree(saving_path)
+
+
+
+def test_ppo_agent_manager_multi_env():
+    saving_path = "rlberry/agents/torch/tests/agentmanager_test_ppo_classic_env"
+
+    # Remove previous save
+    if os.path.exists(saving_path):
+        shutil.rmtree(saving_path)
+    assert not os.path.exists(saving_path)
+
+    test_agent_manager = AgentManager(
+        PPOAgent,  # The Agent class.
+        (
+            gym_make,
+            dict(
+                id="CartPole-v0",
+            )
+        ),  # The Environment to solve.
+        init_kwargs=dict(  # Where to put the agent's hyperparameters
+            learning_rate=1e-4,
+            optimizer_type="ADAM",
+            n_envs=3,
+        ),
+        fit_budget=1000,  # The number of interactions between the agent and the environment during training.
+        eval_kwargs=dict(
+            eval_horizon=50
+        ),  # The number of interactions between the agent and the environment during evaluations.
+        n_fit=1,  # The number of agents to train. Usually, it is good to do more than 1 because the training is stochastic.
+        agent_name="test_ppo_classic_env",  # The agent's name.
+        output_dir=saving_path,
+    )
+
+    test_agent_manager.fit(budget=1000)
+
+    # test the save function
+    test_agent_manager.save()
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    path_to_load = next(pathlib.Path(saving_path).glob("**/*.pickle"))
+    loaded_agent_manager = AgentManager.load(path_to_load)
+    assert loaded_agent_manager
+
+    # test the agent
+    state = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent_manager.get_agent_instances()[0].policy(state)
+        next_s, _, done, test = test_load_env.step(action)
+        if done:
+            break
+        state = next_s
+
+    shutil.rmtree(saving_path)
+
+
+
+def test_ppo_agent_manager_multi_fit():
+    saving_path = "rlberry/agents/torch/tests/agentmanager_test_ppo_classic_env"
+
+    # Remove previous save
+    if os.path.exists(saving_path):
+        shutil.rmtree(saving_path)
+    assert not os.path.exists(saving_path)
+
+    test_agent_manager = AgentManager(
+        PPOAgent,  # The Agent class.
+        (
+            gym_make,
+            dict(
+                id="CartPole-v0",
+            ),
+        ),  # The Environment to solve.
+        init_kwargs=dict(  # Where to put the agent's hyperparameters
+            learning_rate=1e-4,
+            optimizer_type="ADAM",
+        ),
+        fit_budget=1000,  # The number of interactions between the agent and the environment during training.
+        eval_kwargs=dict(
+            eval_horizon=50
+        ),  # The number of interactions between the agent and the environment during evaluations.
+        n_fit=1,  # The number of agents to train. Usually, it is good to do more than 1 because the training is stochastic.
+        agent_name="test_ppo_classic_env",  # The agent's name.
+        output_dir=saving_path,
+    )
+
+    test_agent_manager.fit(budget=1000)
+    test_agent_manager.fit(budget=1000)
+
+    # test the save function
+    test_agent_manager.save()
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    path_to_load = next(pathlib.Path(saving_path).glob("**/*.pickle"))
+    loaded_agent_manager = AgentManager.load(path_to_load)
+    assert loaded_agent_manager
+
+    # test the agent
+    state = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent_manager.get_agent_instances()[0].policy(state)
+        next_s, _, done, test = test_load_env.step(action)
+        if done:
+            break
+        state = next_s
+
+    shutil.rmtree(saving_path)
+
+
+def test_ppo_agent_manager_multi_env_multi_fit():
+    saving_path = "rlberry/agents/torch/tests/agentmanager_test_ppo_classic_env"
+
+    # Remove previous save
+    if os.path.exists(saving_path):
+        shutil.rmtree(saving_path)
+    assert not os.path.exists(saving_path)
+
+    test_agent_manager = AgentManager(
+        PPOAgent,  # The Agent class.
+        (
+            gym_make,
+            dict(
+                id="CartPole-v0",
+            )
+        ),  # The Environment to solve.
+        init_kwargs=dict(  # Where to put the agent's hyperparameters
+            learning_rate=1e-4,
+            optimizer_type="ADAM",
+            n_envs=3,
+        ),
+        fit_budget=1000,  # The number of interactions between the agent and the environment during training.
+        eval_kwargs=dict(
+            eval_horizon=50
+        ),  # The number of interactions between the agent and the environment during evaluations.
+        n_fit=1,  # The number of agents to train. Usually, it is good to do more than 1 because the training is stochastic.
+        agent_name="test_ppo_classic_env",  # The agent's name.
+        output_dir=saving_path,
+    )
+
+    test_agent_manager.fit(budget=1000)
+    test_agent_manager.fit(budget=1000)
+
+    # test the save function
+    test_agent_manager.save()
+    assert os.path.exists(saving_path)
+
+    # test the loading function
+    test_load_env = gym_make("CartPole-v0")
+    path_to_load = next(pathlib.Path(saving_path).glob("**/*.pickle"))
+    loaded_agent_manager = AgentManager.load(path_to_load)
+    assert loaded_agent_manager
+
+    # test the agent
+    state = test_load_env.reset()
+    for tt in range(50):
+        action = loaded_agent_manager.get_agent_instances()[0].policy(state)
+        next_s, _, done, test = test_load_env.step(action)
+        if done:
+            break
+        state = next_s
+
+    shutil.rmtree(saving_path)
+
