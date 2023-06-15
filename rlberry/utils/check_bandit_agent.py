19ccdce2dec382b418f13096d9a0b1d777e6112c
TimotheeMathieu <timothee.mathieu@inria.fr>
2023-06-15 14:59:59 +0200

    [MRG] implement queues in stochastic bandits (#309)
    * implement queues in stochastic bandits
    
    * trigger ci
    
    * various minor changes to fix reproducibility and regressiont tests
    
    * new seeder
    
    * Revert "new seeder"
    
    This reverts commit 0885f591e8898871f4700d35c57a48965f194458.

--- rlberry/utils/check_bandit_agent.py
+++ rlberry/utils/check_bandit_agent.py
@@ -49,8 +49,7 @@ def check_bandit_agent(Agent, environment=BernoulliBandit, seed=42):
 
     agent1.fit()
     agent2.fit()
-    env = env_ctor(**env_kwargs)
-    state = env.reset()
+    state = 0
     result = True
     for _ in range(5):
         # test reproducibility on 5 actions
