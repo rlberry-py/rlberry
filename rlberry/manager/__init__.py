3fb76b61f4976651f71cc9765726166944e51269
TimotheeMathieu <timothee.mathieu@inria.fr>
2023-10-22 14:24:17 +0200

    initial venv tool

--- rlberry/manager/__init__.py
+++ rlberry/manager/__init__.py
@@ -3,6 +3,7 @@ from .multiple_managers import MultipleManagers
 from .remote_experiment_manager import RemoteExperimentManager
 from .evaluation import evaluate_agents, plot_writer_data, read_writer_data
 from .comparison import compare_agents
+from .venv_tools import with_venv, run_xp
 
 # (Remote)AgentManager alias for the (Remote)ExperimentManager class, for backward compatibility
 AgentManager = ExperimentManager
