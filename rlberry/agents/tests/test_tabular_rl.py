fe1f18033cccd00326646645bf061914f425797a
Waris Radji <wradji64@gmail.com>
2023-11-06 11:23:45 +0100

    Discard changes to rlberry/agents/tests/test_tabular_rl.py

---
+++ rlberry/agents/tests/test_tabular_rl.py
@@ -0,0 +1,33 @@
+import pytest
+from rlberry.agents import QLAgent, SARSAAgent
+from rlberry.envs import GridWorld
+
+
+@pytest.mark.parametrize(
+    "exploration_type, exploration_rate",
+    [("epsilon", 0.5), ("boltzmann", 0.5), (None, None)],
+)
+def test_ql(exploration_type, exploration_rate):
+    env = GridWorld(walls=(), nrows=5, ncols=5)
+    agent = QLAgent(
+        env, exploration_type=exploration_type, exploration_rate=exploration_rate
+    )
+    agent.fit(budget=50)
+    agent.policy(env.observation_space.sample())
+    agent.reset()
+    assert not agent.Q.any()
+
+
+@pytest.mark.parametrize(
+    "exploration_type, exploration_rate",
+    [("epsilon", 0.5), ("boltzmann", 0.5), (None, None)],
+)
+def test_sarsa(exploration_type, exploration_rate):
+    env = GridWorld(walls=(), nrows=5, ncols=5)
+    agent = SARSAAgent(
+        env, exploration_type=exploration_type, exploration_rate=exploration_rate
+    )
+    agent.fit(budget=50)
+    agent.policy(env.observation_space.sample())
+    agent.reset()
+    assert not agent.Q.any()
