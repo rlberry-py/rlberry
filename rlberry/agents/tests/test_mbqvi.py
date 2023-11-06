8186e5a7daaac9acd5252a0833e8e6e0925a53b5
Waris Radji <wradji64@gmail.com>
2023-11-06 11:23:30 +0100

    Discard changes to rlberry/agents/tests/test_mbqvi.py

---
+++ rlberry/agents/tests/test_mbqvi.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+from rlberry.seeding import Seeder
+from rlberry.agents.mbqvi import MBQVIAgent
+from rlberry.envs.finite import FiniteMDP
+
+
+@pytest.mark.parametrize("S, A", [(5, 2), (10, 4)])
+def test_mbqvi(S, A):
+    rng = Seeder(123).rng
+
+    for sim in range(5):
+        # generate random MDP with deterministic transitions
+        R = rng.uniform(0.0, 1.0, (S, A))
+        P = np.zeros((S, A, S))
+        for ss in range(S):
+            for aa in range(A):
+                ns = rng.integers(0, S)
+                P[ss, aa, ns] = 1
+
+        # run MBQVI and check exactness of estimators
+        env = FiniteMDP(R, P)
+        agent = MBQVIAgent(env, n_samples=1)
+        agent.fit()
+        assert np.abs(R - agent.R_hat).max() < 1e-16
+        assert np.abs(P - agent.P_hat).max() < 1e-16
