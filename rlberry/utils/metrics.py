5f67d1df56c9975a4a6e41b86e129efa601fc179
Waris Radji <wradji64@gmail.com>
2023-11-06 14:42:11 +0100

    Discard changes to rlberry/utils/metrics.py

--- rlberry/utils/metrics.py
+++ rlberry/utils/metrics.py
@@ -3,7 +3,7 @@ from rlberry.utils.jit_setup import numba_jit
 
 
 @numba_jit
-def metric_lp(x: np.ndarray, y: np.ndarray, p: int, scaling: np.ndarray) -> np.ndarray:
+def metric_lp(x, y, p, scaling):
     """
     Returns the p-norm:  || (x-y)/scaling||_p
 
