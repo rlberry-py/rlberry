0e44d1e32e1d40bbd71d771c52c60d3723e78563
Waris Radji <wradji64@gmail.com>
2023-11-06 11:48:57 +0100

    Update and remove some files in agents and envs

--- rlberry/agents/__init__.py
+++ rlberry/agents/__init__.py
@@ -2,16 +2,3 @@
 from .agent import Agent
 from .agent import AgentWithSimplePolicy
 from .agent import AgentTorch
-
-# Basic agents (in alphabetical order)
-# basic = does not require torch, jax, etc...
-from .adaptiveql import AdaptiveQLAgent
-from .dynprog import ValueIterationAgent
-from .kernel_based import RSUCBVIAgent, RSKernelUCBVIAgent
-from .linear import LSVIUCBAgent
-from .mbqvi import MBQVIAgent
-from .optql import OptQLAgent
-from .psrl import PSRLAgent
-from .rlsvi import RLSVIAgent
-from .ucbvi import UCBVIAgent
-from .tabular_rl import QLAgent, SARSAAgent
--- rlberry/envs/__init__.py
+++ rlberry/envs/__init__.py
@@ -1,6 +1,4 @@
 from .gym_make import gym_make, atari_make
 from .basewrapper import Wrapper
-from .classic_control import Acrobot, MountainCar, Pendulum, SpringCartPole
-from .finite import Chain, FiniteMDP, GridWorld
 from .interface import Model
 from .pipeline import PipelineEnv
