21b1bc3e985132d8ba240cf7558b1dc196203a87
TimotheeMathieu <timothee.mathieu@inria.fr>
2023-11-07 15:39:12 +0100

    New try on Page (#387)
    * page
    
    * typo
    
    * install rlberry
    
    * install rlberry
    
    * fix yaml spaces
    
    * trigger condition
    
    * test ci
    
    * trigger page
    
    * trigger page
    
    * trigger page

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "rlberry"
-copyright = "2022, rlberry team"
+copyright = "2023, rlberry team"
 author = "rlberry team"
 
 
