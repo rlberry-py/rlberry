47f6d0684dd66374287abc7247f76859e5ea4235
TimotheeMathieu <TimotheeMathieu@users.noreply.github.com>
2023-11-07 13:57:42 +0000

    This commit squashes everything that was used to setup the new documentation workflow with github page.

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 
+
 import sphinx_gallery  # noqa
 from sphinx_gallery.sorting import ExplicitOrder
 
@@ -22,9 +23,8 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "rlberry"
-copyright = "2022, rlberry team"
+copyright = "2023, rlberry team"
 author = "rlberry team"
-
 
 ver_file = os.path.join("../rlberry", "_version.py")
 with open(ver_file) as f:
@@ -51,19 +51,7 @@ extensions = [
 ]
 
 myst_enable_extensions = ["amsmath"]
-# myst_enable_extensions = [
-#     "amsmath",
-#     "colon_fence",
-#     "deflist",
-#     "dollarmath",
-#     "fieldlist",
-#     "html_admonition",
-#     "html_image",
-#     "replacements",
-#     "smartquotes",
-#     "substitution",
-#     "tasklist",
-# ]
+
 
 autodoc_default_options = {
     "members": True,
