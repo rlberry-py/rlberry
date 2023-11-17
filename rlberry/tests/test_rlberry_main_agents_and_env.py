9c60fbeb9e8d3655ed9f7411289c7f0079c909bd
JulienT01 <julien.tj@gmail.com>
2023-11-17 15:13:40 +0100

    add tests to better coverage (env with action space in Box)

--- rlberry/tests/test_rlberry_main_agents_and_env.py
+++ rlberry/tests/test_rlberry_main_agents_and_env.py
@@ -7,11 +7,11 @@ Tests some agent and env from rlberry only (no rlberry-scool or rlberry research
 
 from rlberry.utils.check_env import check_env, check_gym_env
 from rlberry.utils.check_agent import check_rl_agent
-from rlberry.envs import gym_make
+from rlberry.envs import gym_make, atari_make
 from rlberry.agents.stable_baselines import StableBaselinesAgent
 from stable_baselines3 import A2C
 
-# from stable_baselines3 import DQN
+from stable_baselines3 import DQN
 import pytest
 
 
@@ -20,27 +20,26 @@ FROZEN_LAKE_CONSTR = (
     dict(id="FrozenLake-v1", wrap_spaces=True, is_slippery=False),
 )
 CART_POLE_CONSTR = (gym_make, dict(id="CartPole-v1", wrap_spaces=True))
+PENDULUM_CONSTR = (gym_make, dict(id="Pendulum-v1", wrap_spaces=True))
+ASTEROIDS_CONSTR = (atari_make, dict(id="ALE/Asteroids-v5", wrap_spaces=True))
 
-ALL_ENVS = [
-    FROZEN_LAKE_CONSTR,
-    CART_POLE_CONSTR,
-]
+
+ALL_ENVS = [FROZEN_LAKE_CONSTR, CART_POLE_CONSTR, PENDULUM_CONSTR, ASTEROIDS_CONSTR]
 
 
 @pytest.mark.parametrize("Env", ALL_ENVS)
 def test_env(Env):
     current_env = Env[0](**Env[1])
     check_env(current_env)
-    # check_rlberry_env(current_env)
     check_gym_env(current_env)
 
 
 A2C_INIT_KWARGS = {"algo_cls": A2C, "policy": "MlpPolicy", "verbose": 1}
-# DQN_INIT_KWARGS = {"algo_cls": DQN, "policy": "MlpPolicy", "verbose": 1}
+DQN_INIT_KWARGS = {"algo_cls": DQN, "policy": "MlpPolicy", "verbose": 1}
 
 AGENTS_WITH_ENV = [
-    (A2C_INIT_KWARGS, CART_POLE_CONSTR),
-    # (DQN_INIT_KWARGS,FROZEN_LAKE_CONSTR),
+    (A2C_INIT_KWARGS, PENDULUM_CONSTR),
+    (DQN_INIT_KWARGS, CART_POLE_CONSTR),
 ]
 
 
