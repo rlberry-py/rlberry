dee14bad20dfca54aee51da744af3e5c672e2401
JulienT01 <julien.tj@gmail.com>
2023-12-14 09:33:13 +0100

    removing useless try/catch

--- rlberry/tests/test_imports.py
+++ rlberry/tests/test_imports.py
@@ -1,14 +1,9 @@
 def test_imports():
-    try:
-        import rlberry  # noqa
-        from rlberry.manager import (  # noqa
-            ExperimentManager,  # noqa
-            evaluate_agents,  # noqa
-            plot_writer_data,  # noqa
-        )  # noqa
-        from rlberry.agents import AgentWithSimplePolicy  # noqa
-        from rlberry.wrappers import WriterWrapper  # noqa
-
-    except Exception:
-        raise RuntimeError("Fail to call get_params on the environment.")
-        assert False
+    import rlberry  # noqa
+    from rlberry.manager import (  # noqa
+        ExperimentManager,  # noqa
+        evaluate_agents,  # noqa
+        plot_writer_data,  # noqa
+    )  # noqa
+    from rlberry.agents import AgentWithSimplePolicy  # noqa
+    from rlberry.wrappers import WriterWrapper  # noqa
