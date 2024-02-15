6ae3b409a699c3d6c8d33fa80dfe1289722b58f2
Timothee Mathieu <timothee.mathieu@inria.fr>
2024-02-15 10:16:58 +0100

    update deps

--- examples/adastop_example.py
+++ examples/adastop_example.py
@@ -22,13 +22,13 @@ managers = [{"agent_class": StableBaselinesAgent,
              "train_env": (env_ctor, env_kwargs),
              "fit_budget":10000,
              "agent_name":"A2C",
-             "init_kwargs":{"algo_cls": A2C, "policy":"MlpPolicy",  "verbose":1}},
+             "init_kwargs":{"algo_cls": A2C, "policy":"MlpPolicy",  "verbose":0}},
             {"agent_class": StableBaselinesAgent,
              "train_env": (env_ctor, env_kwargs),
              "agent_name":"PPO",
              "fit_budget":10000,
-             "init_kwargs":{"algo_cls": PPO,"policy":"MlpPolicy", "verbose":1}},
+             "init_kwargs":{"algo_cls": PPO,"policy":"MlpPolicy", "verbose":0}},
             ]
 
-comparator = AdastopComparator()
+comparator = AdastopComparator(n=10,K=5)
 comparator.compare(managers)
