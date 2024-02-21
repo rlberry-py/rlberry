2b37773edb97729e85a932a838d050ed7a1d0864
Timothee Mathieu <timothee.mathieu@inria.fr>
2024-02-21 13:05:36 +0000

    comparisons

--- rlberry/manager/tests/test_comparisons.py
+++ rlberry/manager/tests/test_comparisons.py
@@ -2,7 +2,7 @@ import pytest
 from rlberry_research.envs import GridWorld
 from rlberry.agents import AgentWithSimplePolicy
 from rlberry.manager import AgentManager
-from rlberry.manager import compare_agents
+from rlberry.manager import compare_agents, AdastopComparator
 
 
 class DummyAgent(AgentWithSimplePolicy):
@@ -74,3 +74,58 @@ def test_compare(method):
         [agent1_pickle, agent2_pickle], method=method, B=10, n_simulations=5
     )
     assert len(df) > 0
+
+
+def test_adastop():
+    # Define train and evaluation envs
+    train_env = (GridWorld, {})
+    eval_env = (GridWorld, {})
+
+    # Parameters
+    eval_kwargs = dict(eval_horizon=10)
+
+    # Run AgentManager
+    agent1 = AgentManager(
+        DummyAgent,
+        train_env,
+        agent_name="Dummy1",
+        eval_env=eval_env,
+        fit_budget=5,
+        eval_kwargs=eval_kwargs,
+        init_kwargs={"eval_val": 0},
+        n_fit=10,
+        seed=123,
+    )
+    agent2 = AgentManager(
+        DummyAgent,
+        train_env,
+        eval_env=eval_env,
+        agent_name="Dummy2",
+        fit_budget=5,
+        eval_kwargs=eval_kwargs,
+        init_kwargs={"eval_val": 10},
+        n_fit=10,
+        seed=123,
+    )
+
+    managers = [
+        {
+            "agent_class": DummyAgent,
+            "train_env": train_env,
+            "fit_budget": 5,
+            "agent_name": "Dummy1",
+            "init_kwargs": {"eval_val": 0},
+        },
+        {
+            "agent_class": DummyAgent,
+            "train_env": train_env,
+            "agent_name": "Dummy2",
+            "fit_budget": 5,
+            "init_kwargs": {"eval_val": 10},
+        },
+    ]
+
+    comparator = AdastopComparator(seed=42)
+    comparator.compare(managers)
+    assert comparator.is_finished
+    assert not ("equal" in comparator.decisions.values())
