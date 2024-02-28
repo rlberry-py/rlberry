be35769d56cad09d8c20c149b59c947a44f3e70b
JulienT01 <julien.tj@gmail.com>
2024-02-28 14:18:43 +0100

    add test Gridworld with pygame

--- rlberry/rendering/tests/test_rendering_interface.py
+++ rlberry/rendering/tests/test_rendering_interface.py
@@ -8,6 +8,7 @@ from rlberry_research.envs.classic_control import Acrobot
 from rlberry_research.envs.classic_control import Pendulum
 from rlberry_scool.envs.finite import Chain
 from rlberry_scool.envs.finite import GridWorld
+from rlberry_scool.agents.dynprog import ValueIterationAgent
 from rlberry_research.envs.benchmarks.grid_exploration.four_room import FourRoom
 from rlberry_research.envs.benchmarks.grid_exploration.six_room import SixRoom
 from rlberry_research.envs.benchmarks.grid_exploration.apple_gold import AppleGold
@@ -132,6 +133,35 @@ def test_write_gif():
         env.step(env.action_space.sample())
     with tempfile.TemporaryDirectory() as tmpdirname:
         saving_path = tmpdirname + "/test_gif.mp4"
+        env.save_gif(saving_path)
+        assert os.path.isfile(saving_path)
+        try:
+            os.remove(saving_path)
+        except Exception:
+            pass
+
+
+def test_gridworld_rendering():
+    env = GridWorld(7, 10, walls=((2, 2), (3, 3)))
+    env.renderer_type = "pygame"
+
+    agent = ValueIterationAgent(env, gamma=0.95)
+    info = agent.fit()
+    print(info)
+
+    env.enable_rendering()
+    observation, info = env.reset()
+    for tt in range(50):
+        action = agent.policy(observation)
+        observation, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+        if done:
+            # Warning: this will never happen in the present case because there is no terminal state.
+            # See the doc of GridWorld for more informations on the default parameters of GridWorld.
+            break
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        saving_path = tmpdirname + "/test_gif.gif"
         env.save_gif(saving_path)
         assert os.path.isfile(saving_path)
         try:
