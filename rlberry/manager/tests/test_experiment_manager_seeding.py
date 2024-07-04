20c5911b412d47de99222cb93a0e4bdf59ce1d0e
JulienT01 <julien.tj@gmail.com>
2024-07-04 11:35:06 +0200

    add test on seeding (no seeds => different results)

--- rlberry/manager/tests/test_experiment_manager_seeding.py
+++ rlberry/manager/tests/test_experiment_manager_seeding.py
@@ -67,3 +67,65 @@ def test_experiment_manager_and_multiple_managers_seeding(env, agent_class):
 
     stats1.clear_output_dir()
     stats2.clear_output_dir()
+
+
+@pytest.mark.parametrize(
+    "env, agent_class",
+    [
+        ((MountainCar, {}), A2CAgent),
+        ((gym_make, {"id": "MountainCar-v0"}), A2CAgent),
+        (
+            (gym.make, {"id": "MountainCar-v0"}),
+            A2CAgent,
+        ),
+    ],
+)
+def test_experiment_manager_and_multiple_managers_No_seeding(env, agent_class):
+    experiment_manager = ExperimentManager(
+        agent_class, env, fit_budget=2, init_kwargs={}, n_fit=6
+    )
+    experiment_manager_test = ExperimentManager(
+        agent_class, env, fit_budget=2, init_kwargs={}, n_fit=6
+    )
+
+    multimanagers = MultipleManagers()
+    multimanagers.append(experiment_manager)
+    multimanagers.append(experiment_manager_test)
+    multimanagers.run()
+
+    stats1, stats2 = multimanagers.managers
+
+    for ii in range(2, experiment_manager.n_fit):
+        traj1 = get_env_trajectory(stats1.agent_handlers[ii - 2].env, horizon=10)
+        traj2 = get_env_trajectory(stats1.agent_handlers[ii - 1].env, horizon=10)
+        traj3 = get_env_trajectory(stats1.agent_handlers[ii].env, horizon=10)
+
+        traj1_test = get_env_trajectory(stats2.agent_handlers[ii - 2].env, horizon=10)
+        traj2_test = get_env_trajectory(stats2.agent_handlers[ii - 1].env, horizon=10)
+        traj3_test = get_env_trajectory(stats2.agent_handlers[ii].env, horizon=10)
+
+        assert not compare_trajectories(traj1, traj2)
+        assert not compare_trajectories(traj1, traj3)
+        assert not compare_trajectories(traj2, traj3)
+        assert not compare_trajectories(traj1, traj1_test)
+        assert not compare_trajectories(traj2, traj2_test)
+        assert not compare_trajectories(traj3, traj3_test)
+
+    for ii in range(2, experiment_manager.n_fit):
+        rand1 = stats1.agent_handlers[ii - 2].seeder.rng.integers(2**32)
+        rand2 = stats1.agent_handlers[ii - 1].seeder.rng.integers(2**32)
+        rand3 = stats1.agent_handlers[ii].seeder.rng.integers(2**32)
+
+        rand1_test = stats2.agent_handlers[ii - 2].seeder.rng.integers(2**32)
+        rand2_test = stats2.agent_handlers[ii - 1].seeder.rng.integers(2**32)
+        rand3_test = stats2.agent_handlers[ii].seeder.rng.integers(2**32)
+
+        assert rand1 != rand2
+        assert rand1 != rand3
+        assert rand2 != rand3
+        assert rand1 != rand1_test
+        assert rand2 != rand2_test
+        assert rand3 != rand3_test
+
+    stats1.clear_output_dir()
+    stats2.clear_output_dir()
